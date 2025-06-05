ef8f4bb92b5e80fd2c0d0bf8fb2190aaa2b58d68
am-ml <aliredamoumneh@gmail.com>
2025-06-05 19:03:39 +0300

    vercel config attempt 4...

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const serverless = require('serverless-http');
 const app = require('./server');
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app); // ✅ default export, not named
+
